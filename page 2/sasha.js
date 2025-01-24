376bab0b4bbf1b1f7c6b94c174ea84fbe33c13c2
sashaghadigaonkar <sashagladdy11@gmail.com>
2025-01-24 22:15:47 +0530

    page 2

---
+++ page 2/sasha.js
@@ -0,0 +1,22 @@
+
+gsap.from(".header-title",{
+    y:-100,
+    opacity:0,
+    duration:1
+})
+
+gsap.from(".nature-card .text", {
+//   y: -100,
+  opacity: 0,
+  duration: 1,
+  delay:1,
+  stagger:0.5
+});
+
+gsap.from(".eco-button", {
+  //   y: -100,
+  opacity: 0,
+  duration: 1,
+  delay: 5,
+});
+
